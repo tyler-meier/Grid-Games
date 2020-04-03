737dd9c8e5d9bcd2e89bcb2b7aa822e4118ab69a
Tanvi Pabby <tp156@duke.edu>
2020-04-02 20:10:39 -0400

    added to engine

--- src/ooga/engine/Engine.java
+++ src/ooga/engine/Engine.java
@@ -1,11 +1,17 @@
 package ooga.engine;
 
+import ooga.data.DataObject;
+
 public class Engine implements EngineBuilder {
     private GridCreator myGridCreator;
     private Validator myValidator;
     private NewCellAdder myNewCellAdder;
     private Cell[][] myGrid;
     private MatchFinder myMatchFinder;
+
+    public Engine(DataObject myData){
+
+    }
 
     @Override
     public Cell[][] updateBoard() {
@@ -15,5 +21,10 @@ public class Engine implements EngineBuilder {
     @Override
     public Cell[][] getGrid() {
         return new Cell[0][];
+    }
+
+    @Override
+    public State getGameState() {
+        return null;
     }
 }
