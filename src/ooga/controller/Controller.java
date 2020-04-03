ea66d7be6fd375a59a641c082096bd8bee6f5095
Juliet Yznaga <jay18@duke.edu>
2020-04-02 19:12:07 -0500

    Stuff

--- src/ooga/controller/Controller.java
+++ src/ooga/controller/Controller.java
@@ -27,7 +27,7 @@ public class Controller extends Application {
 
     private void newWindow(Stage stage){
         Player player = new Player(stage);
-        player.setLoginButton((UserLogin) (username, password) -> data.validUser(username, password)); //takes a UserLogin functional interface
+        player.setLoginButton((UserLogin) (username, password) -> data.getPlayerProfile(username, password)); //takes a UserLogin functional interface
         player.setCreateUserButton((UserLogin) (username, password) -> data.createUser(username, password)); //takes a UserLogin functional interface
         player.setGameTypeButton((UserLogin) (username, gameType) -> data.hasSavedGame(username, gameType));
 
