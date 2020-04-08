cb24d80f8fff56c26ae07fbcb54a5a4ab2ff4ea4
Alyssa Shin <as895@duke.edu>
2020-04-08 22:34:36 +0900

    added rectangle views as placeholders, completed gridview class

--- src/ooga/player/GridView.java
+++ src/ooga/player/GridView.java
@@ -3,6 +3,9 @@ package ooga.player;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import ooga.engine.grid.Grid;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -11,15 +14,29 @@ import java.util.ResourceBundle;
 public class GridView {
     private static final String RESOURCES = "ooga/player/Resources/Properties";
     private static final String DEFAULT_RESOURCE_PACKAGE = RESOURCES.replace("/", ".");
-
     private static final String IMAGERESOURCES = "ooga/player/Resources/Images";
     private static final String DEFAULT_IMAGERESOURCE_PACKAGE = RESOURCES.replace("/", ".");
-
+    private int myGridSize;
     private ResourceBundle myResources;
 
-    public GridView(String gameType) {
+    public GridView(String gameType, int gridSize) {
         myResources = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + gameType);
+        myGridSize = gridSize;
     }
+
+//    //code for the method to set up the front end grid with binding--move this to where it makes the most sense
+//    //when you're ready to use it, rename things and ask if anything is unclear
+//    public void setGrid(Grid grid) { // ideally we'll be able to make this run one cell at a time
+//        //so we don't need it to be a 2D array, or we just make a Grid object in back end
+//        myGrid = new UICell[grid.getRows()][grid.getColumns]();
+//        for (int r = 0; r < myGrid.length; r++) {
+//            for (int c = 0; c < myGrid[0].length; c++) {
+//                myGrid[r][c] = new UICell(grid.getCell(r, c);
+//                // set property so that front end stops users from making a new move while one is in motion
+//                myGrid[r][c].setMoveInProgress(grid.getInProgressProperty());
+//            }
+//        }
+//    }
 
     /**
      * return grid for the gameplay
@@ -31,9 +48,13 @@ public class GridView {
         GridPane grid = new GridPane();
         grid.setGridLinesVisible(true);
 
+        int myCellWidth = myGridSize/colnum;
+        int myCellHeight = myGridSize/rownum;
+
         for (int row = 0; row < rownum; row++) {
             for (int col = 0; col < colnum; col++) {
-
+                Rectangle rec = new Rectangle(myCellWidth, myCellHeight, Color.WHITE);
+                grid.getChildren().add(rec);
             }
         }
 
@@ -50,5 +71,6 @@ public class GridView {
         ImageView imageview = new ImageView(image);
         return imageview;
     }
+
 
 }
