d079086e6c6067382052fcafb020ccfb117038e2
Tyler Meier <tkm22@duke.edu>
2020-04-07 22:48:38 -0400

    got everything on the screen for the login page, just need to have it working now

--- src/ooga/player/screens/LoginScreen.java
+++ src/ooga/player/screens/LoginScreen.java
@@ -1,50 +1,54 @@
 package ooga.player.screens;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
+import java.util.ResourceBundle;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import ooga.controller.UserLogin;
 import ooga.data.UserProfile;
 
-import java.util.Map;
-
 public class LoginScreen {
 
   private static final int DIMENSION = 500;
+  private static final String RESOURCES = "ooga/player/Resources/";
+  private static final String DEFAULT_RESOURCE_PACKAGE = RESOURCES.replace("/", ".");
+  private static final String BUTTON_STRINGS = DEFAULT_RESOURCE_PACKAGE + "ButtonCreation";
+  private static final String STYLESHEET = "default.css";
 
   private Scene loginScreen;
   private Label loginLabel;
   private BorderPane myBorderPane;
   private VBox myCenterVBox;
-  private Button goButton, newProfileButton, guestButton;
+  private Button loginButton, newProfileButton, guestButton;
+  private ResourceBundle myResources;
   private UserProfile userData;
+  private TextField username, password;
   // set these guys at some point from user input
-  private String username;
-  private String password;
+  private String usernameString;
+  private String passwordString;
 
   public LoginScreen(){
     myCenterVBox = new VBox();
     myBorderPane = new BorderPane();
+    myResources = ResourceBundle.getBundle(BUTTON_STRINGS);
   }
 
   public Scene setUpScene(){
     setupLabel();
     setupLogin();
     setUpOptions();
-    myCenterVBox.getChildren().addAll(loginLabel, goButton, guestButton, newProfileButton);
+    myCenterVBox.getChildren().addAll(loginLabel, username, password, loginButton, guestButton, newProfileButton);
     myBorderPane.setCenter(myCenterVBox);
     loginScreen = new Scene(myBorderPane, DIMENSION, DIMENSION);
     return loginScreen;
   }
 
   public void setLoginButton(UserLogin userLogin){
-    goButton.setOnMouseClicked(e -> {
-      userData = userLogin.getProfile(username, password);
+    loginButton.setOnMouseClicked(e -> {
+      userData = userLogin.getProfile(usernameString, passwordString);
     });
   }
 
@@ -53,20 +57,27 @@ public class LoginScreen {
   }
 
   private void setupLogin(){
-    //need to add the username and password type in stuff
+    username = new TextField();
+    password = new TextField();
+
+    username.setPromptText("Username");
+    password.setPromptText("Password");
+
+    username.getText();
+    password.getText();
   }
 
   private void setUpOptions(){
-    goButton = makeButton("Go!");
-    guestButton = makeButton("Continue as Guest"); //need to fix strings
-    newProfileButton = makeButton("New? Creat New Profile");
+    loginButton = makeButton("LoginButtonCommand");
+    guestButton = makeButton("GuestButtonCommand");
+    newProfileButton = makeButton("NewProfileCommand");
 
     //need to add the start as guest, new player, and go buttons (need to do stuff with buttons)
   }
 
   private Button makeButton(String text) {
     Button newButton = new Button();
-    newButton.setText(text);
+    newButton.setText(myResources.getString(text));
     //newButton.setOnAction(handler);
     return newButton;
   }
