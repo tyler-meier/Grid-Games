8f1990ed07c6a1ceb9423033a79d5023236de774
Alyssa Shin <as895@duke.edu>
2020-04-09 09:38:46 +0900

    fixed errors

--- src/ooga/player/GridView.java
+++ src/ooga/player/GridView.java
@@ -14,14 +14,14 @@ import java.util.ResourceBundle;
 public class GridView {
     private static final String RESOURCES = "ooga/player/Resources/";
     private static final String DEFAULT_RESOURCE_PACKAGE = RESOURCES.replace("/", ".");
-    private static final String IMAGERESOURCES = "ooga/player/Resources/Images/";
-    private static final String DEFAULT_IMAGERESOURCE_PACKAGE = RESOURCES.replace("/", ".");
     private int myGridSize;
+    private String myGameType;
     private ResourceBundle myResources;
 
     public GridView(String gameType, int gridSize) {
         myResources = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + gameType);
         myGridSize = gridSize;
+        myGameType = gameType;
     }
 
 //    //code for the method to set up the front end grid with binding--move this to where it makes the most sense
@@ -36,16 +36,17 @@ public class GridView {
 
         for (int row = 0; row < backendGrid.getRows(); row++) {
             for (int col = 0; col < backendGrid.getCols(); col++) {
-//                Rectangle rec = new Rectangle(myCellWidth, myCellHeight, Color.WHITE);
-//                rec.setStroke(Color.BLACK);
-//                rec.setStrokeWidth(1);
-//                int state = backendGrid.getCell(row,col).getMyState();
-                //ImageView myImage = getImage(state);
-                UICell currCell = new UICell(backendGrid.getCell(row, col));
+                Rectangle rec = new Rectangle(myCellWidth, myCellHeight, Color.WHITE);
+                rec.setStroke(Color.BLACK);
+                rec.setStrokeWidth(1);
+                int state = backendGrid.getCell(row,col).getMyState();
+                UICell currCell = new UICell(backendGrid.getCell(row, col), myGameType);
+                Image myImage = currCell.getImage(state);
+                ImageView myImageView = new ImageView(myImage);
                 currCell.setMoveInProgress(backendGrid.getInProgressProperty());
-                GridPane.setRowIndex(currCell.getImage(), col * myCellWidth);
-                GridPane.setColumnIndex(currCell.getImage(), row * myCellHeight);
-                myGrid.getChildren().add(currCell.getImage());
+                GridPane.setRowIndex(myImageView, col * myCellWidth);
+                GridPane.setColumnIndex(myImageView, row * myCellHeight);
+                myGrid.getChildren().add(myImageView);
             }
         }
         return myGrid;
