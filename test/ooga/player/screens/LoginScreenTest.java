24c2d915e49b71910c2b1991ff46f85b09f63e71
Juliet Yznaga <jay18@duke.edu>
2020-04-16 20:22:23 -0500

    frontend test

--- test/ooga/player/screens/LoginScreenTest.java
+++ test/ooga/player/screens/LoginScreenTest.java
@@ -2,6 +2,13 @@ package ooga.player.screens;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class LoginScreenTest {
 
+  @Test
+  void editProfileTest() {
+      assertEquals(true , true);
+    assertEquals(true , false);
+  }
 }
