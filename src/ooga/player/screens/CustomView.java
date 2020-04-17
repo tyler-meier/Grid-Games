1ac7b8b63f75d7ca6cb57b2f5891aa1a555454c4
Alyssa Shin <as895@duke.edu>
2020-04-18 01:12:18 +0900

    added button labels to resource

--- src/ooga/player/screens/CustomView.java
+++ src/ooga/player/screens/CustomView.java
@@ -1,8 +1,10 @@
 package ooga.player.screens;
 
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
