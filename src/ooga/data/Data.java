a7899626cd3778344a1064d2d5e395337bcabd54
Juliet Yznaga <jay18@duke.edu>
2020-04-22 21:47:48 -0500

    Reset button working

--- src/ooga/data/Data.java
+++ src/ooga/data/Data.java
@@ -216,9 +216,9 @@ public class Data implements DataLink {
     gamePath = String.format(LEVEL_PATH_SKELETON, gameType, level);
     if(!KNOWN_GAME_TYPES.contains(gameType)  && username.equals(GUEST_USER))
     {
-      gamePath = String.format(DEFAULT_CREATED_GAME_PATH_SKELETON, username, gameType);
+      gamePath = String.format(DEFAULT_CREATED_GAME_PATH_SKELETON, currentUser.getUsername(), gameType);
     }
-    if(!KNOWN_GAME_TYPES.contains(gameType) | (!username.equals(GUEST_USER) && level == LOAD_SAVED_GAME))
+    else if(!KNOWN_GAME_TYPES.contains(gameType) | (!username.equals(GUEST_USER) && level == LOAD_SAVED_GAME))
     {
       gamePath = currentUser.getSavedGame(gameType);
     }
