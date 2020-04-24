abef24ae935d97468c1e9a641a8a778537318080
Tanvi Pabby <tp156@duke.edu>
2020-04-24 19:39:39 -0400

    small change to open finder test

--- test/ooga/engine/matchFinder/OpenFinderTest.java
+++ test/ooga/engine/matchFinder/OpenFinderTest.java
@@ -34,7 +34,7 @@ class OpenFinderTest {
         MatchFinder matchFinder = new OpenFinder();
         Map<String, String> engineAttributes = data.getEngineAttributes("CandyCrush");
         Map<String, String> gameAttributes = data.getGameLevelAttributes("Guest", "CandyCrush", 1);
-        Grid myGrid = new Grid(gameAttributes, validator, matchFinder);
+        Grid myGrid = new Grid(engineAttributes, validator, matchFinder);
         myGrid.setNewGame(initialConfig, gameAttributes, null);
         // now we have a grid with states and everything
         List<Cell> ret = new ArrayList<>();
