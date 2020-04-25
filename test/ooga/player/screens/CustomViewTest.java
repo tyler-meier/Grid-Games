d55b4a733b9ecc85479edeba3b4ec24d9426fe1e
Alyssa Shin <as895@duke.edu>
2020-04-25 18:25:37 +0900

    added test for custom view screen

--- test/ooga/player/screens/CustomViewTest.java
+++ test/ooga/player/screens/CustomViewTest.java
@@ -2,19 +2,25 @@ package ooga.player.screens;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import ooga.player.Player;
 import ooga.util.DukeApplicationTest;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomViewTest extends DukeApplicationTest {
-    private static final String TITLE = "Preferences";
-    private static final int DIMENSION = 650;
     private Player myPlayer;
     private CustomView myCustomView;
+    private Scene myScene;
     private Button defaultMode;
     private Button darkMode;
     private Button rainbowMode;
@@ -25,18 +31,19 @@ class CustomViewTest extends DukeApplicationTest {
     public void start(Stage stage) {
         myPlayer = new Player(stage);
         myCustomView = new CustomView(myPlayer);
-
-        stage.setScene(myCustomView.finishStyling(myCustomView.makePanel()));
+        myScene = myCustomView.finishStyling(myCustomView.makePanel());
+        stage.setScene(myScene);
 
         defaultMode = lookup("#default").query();
         darkMode = lookup("#dark").query();
         rainbowMode = lookup("#rainbow").query();
         greenMode = lookup("#green").query();
         waveMode = lookup("#wave").query();
-}
+    }
+
     //checks button images and texts
     @Test
-    void checkButtons() {
+    void checkButtons() throws FileNotFoundException {
         assertEquals(defaultMode.getText(), "Candy Mode");
         assertEquals(darkMode.getText(), "Dark Mode");
         assertEquals(rainbowMode.getText(), "Rainbow Mode");
